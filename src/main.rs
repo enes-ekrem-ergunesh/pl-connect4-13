7f88eef89b8a3b8044724cd7e8994f7db0324045
enesekremergunesh <enes.ekrem.ergunes@stu.ibu.edu.ba>
2023-01-16 13:16:15 +0100

    add matrix module to manipulate game data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// pub mod algorithms;
+// pub mod algorithm_tester;
+// pub mod practice;
+// pub mod practice_main;
+pub mod matrix;
+
+
 fn main() {
-    println!("Hello, world!");
+    // crate::practice_main::practice_main::main(); // this is for practice purposes
+    
+    println!("Data before: {}", matrix::read(0, 2));
+
+    matrix::write(0, 2, matrix::Element::Red);
+
+    println!("Data after: {}", matrix::read(0, 2));
+    
+    
 }
